0daa420dee4371418221a22b2245dd4ee1720b1f
Bruce Dawson <brucedawson@cygnus-software.com>
2021-05-29 17:02:47 -0700

    Make RandomWallpaper.py self-contained
    With this change RandomWallpaper.py now creates the database which it
    uses. It creates the database whenever the number of images found
    changes, and writes the database if it has changed.
    
    It uses the PIL/Pillow module to extract ratings from .jpg files.

--- bin/RandomWallpaper.py
+++ bin/RandomWallpaper.py
@@ -13,38 +13,133 @@
 # limitations under the License.
 
 """
-This script randomly selects a photo from a list of files.
+This script randomly selects a 5-star photo from the user's pictures folder or
+the public pictures folder.
 
-The list of files has a heading line and subsequent lines have tab-separated
-columns where the first column is the file name and the third column is a rating
-that goes from 0 to 5 stars. This script only selects from photos that are
-five-star rated.
+This script uses PIL/Pillow to scan the images and extract rating tags. It
+caches the results in a text file that holds the path, last-modified time, and
+rating (tab separated). It can either update the database occasionally (every
+now and then) or it can get the file list and update the database whenever the
+count changes.
 
-A list of selected photos is retained, which could be used to avoid repetition,
-but given a sufficiently large number of photos this shouldn't be a problem.
+A list of the photos that have been displayed is kept. This list could be used
+to avoid repetition, but given a sufficiently large number of photos there
+shouldn't be a problem with repetition so this has not been implemented. Instead
+the list of displayed photos is simply retained so that the user can find out
+what pictures have been shown.
 
-This can be set as a scheduled task. In this context it is important to run it
-using pythonw.exe so that no console window pops up.
+This can be set as a scheduled task that runs every five minutes or so. In this
+context it is important to run it using pythonw.exe so that no console window
+pops up.
 """
 
 import ctypes
 import os
+# To make this available you need to install Pillow with this command:
+# pip3 install pillow
+from PIL import Image, ExifTags
 import random
 import sys
+import time
+
+def GetPictures(directories):
+  """
+  Scan the specified directories for .jpg files and return a list of file paths.
+  """
+  result = []
+  for directory in directories:
+    for root, dirs, files in os.walk(directory, topdown=False):
+      for name in files:
+        ext = os.path.splitext(name)[1].lower()
+        if ext.lower() in ['.jpg']:
+          result.append(os.path.join(root, name))
+  return result
 
 def main():
+  verbose = True
+  start = time.time()
+  # Use the documents directory or localappdata (shadow version) to store the
+  # database of files and their attributes.
+
   # This is not, apparently, a completely robust way to get the user's documents
   # directory, but the alternatives are much messier and not worth it to me.
-  documents = os.path.expanduser(r'~\Documents')
-  database = os.path.join(documents, 'PhotoDatabase.txt')
-  lines = open(database, 'rb').read().decode('utf-16').splitlines()
-  # Filter to just the 5-star photos. This also removes the heading line.
-  filtered = [line for line in lines if line.split('\t')[2] == '5']
+  database_dir = os.path.expanduser(r'~\Documents')
+
+  # Note that Python reads/writes from a shadow version of localappdata due to
+  # managed app restrictions, which makes the database and the recent files
+  # difficult to view.
+  #database_dir = os.environ['localappdata']
+  database = os.path.join(database_dir, 'WallpaperPhotoDatabase.txt')
+  try:
+    lines = open(database, 'rb').read().decode('utf-16').splitlines()
+  except:
+    # If the database doesn't exist then create an empty one.
+    lines = []
+
+  old_database = {}
+  for line in lines:
+    path, mtime, rating = line.split('\t')
+    old_database[path] = (float(mtime), int(rating))
+
+  mypictures = os.path.expanduser(r'~\Pictures')
+  publicpictures = os.path.normpath(os.path.expanduser(r'~\..\public\Pictures'))
+
+  # Scanning the entire picture directory is fast if it is cached, and gives a
+  # quite reliable way to detect when new pictures appear. But it is not free,
+  # especially on slow hard drives. Set detect_changes to True to get update
+  # whenever the file count changes.
+  detect_changes = True
+  if detect_changes:
+    picture_list = GetPictures([mypictures, publicpictures])
+
+  # Recreate the database if it doesn't exist, and every now and then.
+  if not old_database or (detect_changes and len(picture_list) != len(old_database)):
+    if verbose:
+      print('Updating database from %d to %d pictures.' % (len(old_database), len(picture_list)))
+
+    if not detect_changes:
+      picture_list = GetPictures([mypictures, publicpictures])
+    # Set a larger decompression bomb level. Could set this to None to disable
+    # compression bomb warnings entirely.
+    Image.MAX_IMAGE_PIXELS = 300000000
+
+    rating_tag = 18246
+
+    new_database = {}
+    count = 0
+    for path in picture_list:
+      count += 1
+      if int(count % 1000) == 0:
+        print('%d out of %d, %s.' % (count, len(picture_list), path))
+      mtime = os.path.getmtime(path)
+      if path in old_database and old_database[path][0] == mtime:
+        new_database[path] = old_database[path]
+      else:
+        with Image.open(path) as img:
+          img_exif = img.getexif()
+          rating = img_exif.get(rating_tag, 0)
+        new_database[path] = (mtime, rating)
+    if new_database != old_database:
+      print('Writing updated database.')
+      output_list = []
+      for key, value in new_database.items():
+        output_list.append('%s\t%.7f\t%d' % (key, value[0], value[1]))
+      output_string = '\n'.join(output_list)
+      with open(database, 'wb') as f:
+        f.write(output_string.encode('utf-16'))
+  else:
+    new_database = old_database
+  # Make sure we don't reference old_database anymore
+  del old_database
+
+  # Filter to just the 5-star photos.
+  filtered = [key for key in new_database.keys() if new_database[key][1] == 5]
+  if verbose:
+    print('Filtered from %d to %d pictures.' % (len(new_database), len(filtered)))
 
   while True:
     # Select a random line.
-    line = random.choice(filtered)
-    path = line.split('\t')[0]
+    path = random.choice(filtered)
     # Try again if the photo doesn't exist for some reason.
     if not os.path.exists(path):
       continue
@@ -59,9 +154,12 @@ def main():
     break
 
   # Record information about the displayed photo.
-  history = os.path.join(documents, 'PhotoWallpaperHistory.txt')
+  history = os.path.join(database_dir, 'PhotoWallpaperHistory.txt')
   with open(history, 'a') as f:
-    f.write('%s\n' % line)
+    f.write('%s\t%.7f\t%d\n' % (path, new_database[path][0], new_database[path][1]))
+  elapsed = time.time() - start
+  if verbose:
+    print('%f s to do work.' % elapsed)
 
 
 if __name__ == '__main__':
